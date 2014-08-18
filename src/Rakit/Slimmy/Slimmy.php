63672417d0048f24b66c70b06aabad418e3a3663
Muhammad Syifa <emsifa@gmail.com>
2014-08-18 18:17:13 +0700

    fixing bug in setup twig

--- src/Rakit/Slimmy/Slimmy.php
+++ src/Rakit/Slimmy/Slimmy.php
@@ -63,7 +63,7 @@ class Slimmy extends Slim {
 		// registering native functions
 		$this->twig->registerUndefinedFunctionCallback(function ($name) {
 			if (function_exists($name)) {
-				return new Twig_Function_Function($name);
+				return new \Twig_Function_Function($name);
 			}
 		
 			return false;
